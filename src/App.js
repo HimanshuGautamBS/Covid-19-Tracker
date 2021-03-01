0027737617951993405a40743c93841f8e255d2c
Himanshu Gautam <curiousgeekyy@gmail.com>
2021-03-01 15:14:02 +0530

    Created a structure and different component of chart, cards, countrypicker

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import {Cards ,Chart ,CountryPicker} from './components'
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                <h1>hello</h1>
+             <Cards />
+             <CountryPicker/>
+             <Chart />
             </div>
         )
     }
