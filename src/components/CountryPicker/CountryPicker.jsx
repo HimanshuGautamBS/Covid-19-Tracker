5fa3ec43190b0d0e5d815b37584bab9342982034
Himanshu Gautam <curiousgeekyy@gmail.com>
2021-03-04 13:28:59 +0530

    added event handler with NativeSelect and passing props

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -3,7 +3,7 @@ import { NativeSelect , FormControl } from "@material-ui/core";
 import styles from "./CountryPicker.module.css";
 import { fetchCountries } from "../../api";
 
-const CountryPicker =()=>{
+const CountryPicker =({handleCountryChange})=>{
   const [fetchedCountries, setfetchedCountries] = useState([])
 
   useEffect(()=>{
@@ -17,8 +17,9 @@ const CountryPicker =()=>{
   console.log(fetchedCountries);
   return(
     <FormControl className={styles.formControl}>
-      <NativeSelect>
+      <NativeSelect defaultValue="" onChange={(e)=>handleCountryChange(e.target.value)}> 
         <option value="global">Global</option>
+        {fetchedCountries.map((country,i)=><option key={i}>{country}</option>)}
       </NativeSelect>
      </FormControl>
    )
